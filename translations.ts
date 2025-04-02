a08c935318e12158cb7211d8d2a44387d6f7699c
WSteeven <wilson972906@gmail.com>
2025-04-02 17:06:24 -0500

    feat: colocando dos idiomas en la pagina

---
+++ translations.ts
@@ -0,0 +1,34 @@
+const translations = {
+    en: {
+        greeting: "¡Hola! Soy Wilson Cordova",
+        title: "Welcome to My Portfolio",
+        description: "I am a Full Stack Developer specialized in Laravel and Vue.js.",
+        contact: "Contact",
+        follow_me: "Follow me on my professional social networks:",
+        know_me: "Know me",
+        github: "GitHub",
+        linkedin: "LinkedIn",
+        email: "Email",
+        submit: "Submit Message",
+        name: "Your Name",
+        email_placeholder: "Your Email",
+        message: "Your Message",
+    },
+    es: {
+        greeting: "Hello! I'm Wilson Cordova",
+        title: "Bienvenido a mi Portafolio",
+        description: "Soy un Desarrollador Full Stack especializado en Laravel y Vue.js.",
+        contact: "Contacto",
+        follow_me: "Sígueme en mis redes sociales profesionales:",
+        know_me: "Conóceme",
+        github: "GitHub",
+        linkedin: "LinkedIn",
+        email: "Correo Electrónico",
+        submit: "Enviar Mensaje",
+        name: "Tu Nombre",
+        email_placeholder: "Tu Correo Electrónico",
+        message: "Tu Mensaje",
+    }
+};
+export default translations;
+
